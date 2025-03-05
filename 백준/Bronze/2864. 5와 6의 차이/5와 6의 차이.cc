baabbd5534f03d27e5fc6e51319a17511fb07d61
Limhs <limhs991013@gmail.com>
2025-03-05 23:11:41 +0900

    [Bronze II] Title: 5와 6의 차이, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string s1, s2;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	cin >> s1 >> s2;
+
+	int s1_size = s1.size();
+	int s2_size = s2.size();
+
+	for (int i = 0; i < s1_size; ++i) 
+		if (s1[i] == '6') 
+			s1[i] = '5';
+
+	for (int i = 0; i < s2_size; ++i)
+		if (s2[i] == '6') 
+			s2[i] = '5';
+
+	cout << stoi(s1) + stoi(s2) << ' ';
+
+	for (int i = 0; i < s1_size; ++i)
+		if (s1[i] == '5') 
+			s1[i] = '6';
+
+	for (int i = 0; i < s2_size; ++i) 
+		if (s2[i] == '5') 
+			s2[i] = '6';
+
+	cout << stoi(s1) + stoi(s2);
+
+	return 0;
+}
