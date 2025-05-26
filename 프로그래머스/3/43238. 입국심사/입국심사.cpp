477d0ca377050b389769e8be01709cbfdabb95f1
Limhs <limhs991013@gmail.com>
2025-05-27 02:56:35 +0900

    [level 3] Title: 입국심사, Time: 54.19 ms, Memory: 6.76 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(int n, vector<int> times) {
+    long long answer = 0;
+    
+    sort(times.begin(), times.end());
+    
+    long long left = 1;
+    long long right = (long long)times.back() * n;
+    
+    answer = right;
+    
+    while(left <= right)
+    {
+        long long mid = (left + right) / 2;
+        long long tmp = 0;
+
+
+        for (int i = 0; i < times.size(); ++i) 
+            tmp += (mid / (long long) times[i]);
+
+        if (tmp >= n) 
+        {
+            right = mid - 1;
+            answer = mid;
+        }
+
+        else 
+            left = mid + 1;
+    }
+    
+    return answer;
+}
