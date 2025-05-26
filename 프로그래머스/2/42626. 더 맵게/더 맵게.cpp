ef418d89a4256cbc3d62b61848d19a47877b2558
Limhs <limhs991013@gmail.com>
2025-05-27 00:33:30 +0900

    [level 2] Title: 더 맵게, Time: 228.67 ms, Memory: 40.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+
+    sort(scoville.begin(), scoville.end());
+    
+    priority_queue<int, vector<int>, greater<int>> pq(scoville.begin(), scoville.end());
+
+    while(pq.size() >= 2 && pq.top() < K)
+    {
+        int minNum1 = pq.top(); 
+        pq.pop();
+        int minNum2 = pq.top();
+        pq.pop();
+        
+        int newScoville = minNum1 + (minNum2 * 2);
+        pq.push(newScoville);
+        ++answer;
+    }
+    
+    if(pq.top() < K)
+    {
+        answer = -1;
+    }
+    
+    return answer;
+}
