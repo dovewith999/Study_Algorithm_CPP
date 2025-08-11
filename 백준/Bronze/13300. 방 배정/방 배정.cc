a65e6f5d32f21756683a40a637565ab0a6fdf356
Limhs <limhs991013@gmail.com>
2025-08-11 12:17:48 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.cc
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -1,33 +1,36 @@
 #include <iostream>
-
 using namespace std;
 
 int N, K;
 int cnt = 0;
-
-int arr[2][6] = { 0, };
-
+int arr[2][6] = { 0 };
 int main()
 {
-	ios::sync_with_stdio(false);
-	cout.tie(nullptr);
-	cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cout.tie(nullptr);
+    cin.tie(nullptr);
 
-	cin >> N >> K;
+    cin >> N >> K;
 
-	for (int i = 0; i < N; ++i)
-	{
-		int gender, grade;
-		cin >> gender >> grade;
+    for (int i = 0; i < N; ++i)
+    {
+        int gender, grade;
+        cin >> gender >> grade;
+        arr[gender][grade - 1]++;
+    }
 
-		if (arr[gender][grade] == 0)
-			++cnt;
 
-		++arr[gender][grade];
+    for (int g = 0; g < 2; ++g)
+    {
+        for (int gr = 0; gr < 6; ++gr)
+        {
+            if (arr[g][gr] > 0)
+            {
+                cnt += (arr[g][gr] + K - 1) / K;
+            }
+        }
+    }
 
-		if (arr[gender][grade] >= K)
-			arr[gender][grade] %= K;
-	}
-
-	cout << cnt << '\n';
+    cout << cnt << '\n';
+    return 0;
 }
