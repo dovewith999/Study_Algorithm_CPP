00a84cd41214fa7dd4932f0090b8b86a1255123a
Limhs <limhs991013@gmail.com>
2025-08-11 12:08:04 +0900

    [Bronze II] Title: 방 배정, Time: 0 ms, Memory: 2020 KB, Score: 2 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int N, K;
+int cnt = 0;
+
+int arr[2][6] = { 0, };
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cout.tie(nullptr);
+	cin.tie(nullptr);
+
+	cin >> N >> K;
+
+	for (int i = 0; i < N; ++i)
+	{
+		int gender, grade;
+		cin >> gender >> grade;
+
+		if (arr[gender][grade] == 0)
+			++cnt;
+
+		++arr[gender][grade];
+
+		if (arr[gender][grade] == 2)
+			arr[gender][grade] = 0;
+	}
+
+	cout << cnt << '\n';
+}
