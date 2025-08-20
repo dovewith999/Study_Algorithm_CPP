720ce7d1c4424a241a66f80e03baac192e6da30e
Limhs <limhs991013@gmail.com>
2025-08-20 11:47:30 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.02 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    
+    string str = to_string(n);
+    
+    for(int i = str.length() - 1; i >= 0; --i)
+    {
+        answer.emplace_back(str[i] - '0');
+    }
+    return answer;
+}
