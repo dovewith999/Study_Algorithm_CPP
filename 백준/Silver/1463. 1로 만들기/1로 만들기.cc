78b8ca1f1985284ef492d208c26ea569e91d962c
Limhs <limhs991013@gmail.com>
2025-08-23 08:57:15 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+
+int DP[1000000];
+
+int main() 
+{
+	int N;
+	std::cin >> N;
+
+	for (int i = 2; i <= N; i++) 
+	{
+		// 1. 1을 뺀다.
+		DP[i] = DP[i - 1] + 1;
+
+		// 2. 2로 나누어지면
+		if (i % 2 == 0)
+		{
+			DP[i] = std::min(DP[i], DP[i / 2] + 1);
+		}
+
+		// 3. 3으로 나누어지면
+		if (i % 3 == 0) 
+		{
+			DP[i] = std::min(DP[i], DP[i / 3] + 1);
+		}
+	}
+
+	std::cout << DP[N];
+
+	return 0;
+}
