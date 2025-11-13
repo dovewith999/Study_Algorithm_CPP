fa5001423b8f8b00dbe54db2477886fdf28332aa
Limhs <limhs991013@gmail.com>
2025-11-13 09:23:14 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <array>
+
+using namespace std;
+
+int solution(vector<vector<int>> lines) {
+    int answer = 0;
+    
+    array<int, 200> arr{}; 
+    
+    for(int i = 0; i < lines.size(); ++i)
+    {
+        for(int j = lines[i][0]; j < lines[i][1]; ++j)
+        {
+            ++arr[j + 100];
+        }
+    }
+    
+    for(int i  = 0; i < 200; ++i)
+    {
+        if(arr[i] >= 2)
+        {
+            ++answer;
+        }
+    }
+    
+    return answer;
+}
